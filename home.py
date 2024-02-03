9d10299493eae28c0d1ecf782b009d4b696680e9
LucassSantoss <lucascross.pereirasantos@gmail.com>
2024-02-03 12:36:37 -0300

    projeto finalizado

--- home.py
+++ home.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-st.text("Hello World")
-print("Hello World")
+print("Iniciando Programa...")
+st.title("Capacidade das escolas")
+st.text("Prefeitura de Bauru")
 
 caminhoDoArquivo = r"C:\Programacao\EngSoft\3oSemestre\cursoPython\dados\capacidade_das_escolas.csv"
-df = pd.read_csv(caminhoDoArquivo)
+df = pd.read_csv(caminhoDoArquivo, sep=';', encoding="utf-8")
 st.table(df)
+
+eixoX = df['Unidade'].nunique()
+eixoY = df['Capacidade'].sum()
+
+fig = px.bar(df, x='Unidade', y='Capacidade')
+st.plotly_chart(fig)
